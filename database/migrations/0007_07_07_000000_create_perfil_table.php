b8180b65f78819b4600c3f93dd65e8356e47a559
Leonardo Prates <ti015@example.com>
2026-04-10 16:33:22 -0300

    Fix: remover arquivos rastreados que deveriam ser ignorados

---
+++ database/migrations/0007_07_07_000000_create_perfil_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::create('perfil', function (Blueprint $table) {
+                $table->id('id');
+                $table->string('nomeEmpresa',250);
+                $table->string('email',250)->unique();
+                $table->string('telefone', 20)->nullable();
+                $table->string('endereco', 250)->nullable();
+                $table->decimal('valorHora', 8, 2)->nullable();
+                $table->hour('inicioExpediente')->nullable();
+                $table->hour('fimExpediente')->nullable();
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
